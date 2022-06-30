291d74492934c64ae4e7dac4dd292cce321fe5aa
GarikVologodskiy <49877717+GarikVologodskiy@users.noreply.github.com>
2022-06-30 22:05:24 +0500

    Epic question

---
+++ src/main/java/PrinceOfDenmark.java
@@ -0,0 +1,10 @@
+public class PrinceOfDenmark {
+    void tortureYourSelf (boolean mind) {
+        if (mind) {
+            System.out.println("I will hate everybody including myself!");
+        }
+        else {
+            System.out.println("I will be a good citizen");
+        }
+    }
+}
---
+++ src/main/java/Test.java
@@ -0,0 +1,7 @@
+public class Test {
+    public static void main(String[] args) throws Exception {
+        boolean toBe = Boolean.valueOf("Fuck it, i'm gonna watch TV");
+        PrinceOfDenmark hamlet = new PrinceOfDenmark();
+        hamlet.tortureYourSelf((toBe && !toBe));
+    }
+}
